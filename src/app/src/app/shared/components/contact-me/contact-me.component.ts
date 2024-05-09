fa7851049efb944688c69d96b7594f3be07437ac
Aninda Dhar <aninda400@gmail.com>
2024-05-09 18:25:15 +0930

    working on contact me

--- src/app/src/app/shared/components/contact-me/contact-me.component.ts
+++ src/app/src/app/shared/components/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-contact-me',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
-
+  contactData: any;
+  constructor(private dataService: DataService){
+    this.dataService.dataContent.subscribe({
+      next: (data: any) => this.contactData = data,
+      error: (error ) => console.error('failed to get contact data', error)
+    })
+  }
 }
