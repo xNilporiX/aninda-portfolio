6e5c660634e92beebbaf4cccf85b24e7e76a4678
Aninda Dhar <aninda.dhar@chamonix.com.au>
2024-09-29 19:14:30 +0930

    need to fix a bunch of stuff

--- src/app/src/app/app.routes.ts
+++ src/app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { WorkExperienceComponent } from './shared/components/work-experience/work-experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',  component: HeaderComponent
+    },
+    {
+        path: 'experience', component: WorkExperienceComponent
+    }
+];
