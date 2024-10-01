cdfd2cf8eee03213940e578e1b9dd221227954c8
Aninda Dhar <aninda.dhar@chamonix.com.au>
2024-10-01 16:24:08 +0930

    using tailwind

--- src/app/src/app/app.component.ts
+++ src/app/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { ContentComponent } from './shared/components/content/content.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DataService } from './services/data/data.service';
-import { WorkExperienceComponent } from './shared/components/work-experience/work-experience.component';
-import { PostsComponent } from './shared/components/posts/posts.component';
-import { ContactMeComponent } from './shared/components/contact-me/contact-me.component';
+import { NavBarComponent } from "./shared/components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, PostsComponent, ContactMeComponent, ContentComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,6 +14,4 @@ export class AppComponent {
   constructor(private dataService: DataService){
     this.dataService.getData();
   }
-
-
 }
---
+++ src/app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'primary-1': '#E3FDFD',
+        'secondary-1': '#CBF1F5',
+        'primary-2': '#A6E3E9',
+        'secondary-2': '#87A2FF'
+      },
+      textColor: {
+        'primary-1': '#433878'
+      }
+    },
+  },
+  plugins: [],
+}
+
