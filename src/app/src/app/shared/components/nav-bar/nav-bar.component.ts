852401f4af6ed51957e47a60b62831a6e80df986
Aninda Dhar <aninda.dhar@chamonix.com.au>
2024-10-06 15:45:42 +1030

    new nav

--- src/app/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  open = false;
 
+  toggle() {
+    this.open = !this.open;
+  }
 }
