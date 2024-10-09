e9ae8ac77b22de042558b08d219cc566c8959fe6
Aninda Dhar <aninda.dhar@chamonix.com.au>
2024-10-09 12:33:33 +1030

    scss improvements

--- src/app/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,7 +12,32 @@ import { CommonModule } from '@angular/common';
 export class NavBarComponent {
   open = false;
 
+  constructor(private router: Router, private elementRef: ElementRef, private renderer: Renderer2) {
+    // Listen for clicks outside the navbar
+    this.renderer.listen('window', 'click', (event: Event) => {
+      if (!this.elementRef.nativeElement.contains(event.target)) {
+        this.open = false; // Close the navbar if the click is outside
+      }
+    });
+  }
+
   toggle() {
     this.open = !this.open;
+  }
+
+  /**
+   * Navigaes to the home page.
+   */
+  navToHome() {
+    this.router.navigate(['/']);
+    this.open = false;
+  }
+
+  /**
+   * Navigates to the Experience page.
+   */
+  navToExperience() {
+    this.router.navigate(['experience']);
+    this.open = false;
   }
 }
--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
         'primary-1': '#E3FDFD',
         'secondary-1': '#CBF1F5',
         'primary-2': '#A6E3E9',
-        'secondary-2': '#87A2FF'
+        'secondary-2': '#87A2FF',
+        'primary-3': '#433878',
+        'secondary-3': '#A594F9'
       },
       textColor: {
         'primary-1': '#433878',
@@ -19,6 +21,14 @@ module.exports = {
         'sm': '25rem',
         'md': '40rem',
         'lg': '80rem',
+      },
+      borderColor: {
+        'primary-1': '#E3FDFD',
+        'secondary-1': '#CBF1F5',
+        'primary-2': '#A6E3E9',
+        'secondary-2': '#87A2FF',
+        'primary-3': '#433878',
+        'secondary-3': '#A594F9'
       }
     },
   },
