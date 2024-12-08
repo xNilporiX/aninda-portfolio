4413a5fc523e9b44ffbbd4442d7f01f5787614e1
Aninda Dhar <aninda.dhar@chamonix.com.au>
2024-12-08 23:39:26 +0600

    nav fix, scroll fix

--- src/app/src/app/shared/components/projects-page/projects-page.component.ts
+++ src/app/src/app/shared/components/projects-page/projects-page.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { ProjectTimelineCardComponent } from "./project-timeline-card/project-timeline-card.component";
-import { SkillsComponent } from "../skills/skills.component";
+import { Component, OnInit } from '@angular/core';
+import { ProjectTimelineCardComponent } from './project-timeline-card/project-timeline-card.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects-page',
   standalone: true,
   imports: [ProjectTimelineCardComponent, SkillsComponent],
   templateUrl: './projects-page.component.html',
-  styleUrl: './projects-page.component.scss'
+  styleUrl: './projects-page.component.scss',
 })
-export class ProjectsPageComponent {
+export class ProjectsPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    const scrollContainer: HTMLElement | null = document.getElementById('scroll-container');
+    if (scrollContainer) {
+      scrollContainer.scrollTo(0, 0);
+    }
+  }
 }
